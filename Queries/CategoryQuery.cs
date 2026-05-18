9857ce77f2a67c8b2f1ab562222c250576cdaa84
Joddy0 <143619313+Joddy0@users.noreply.github.com>
2026-05-18 21:27:49 +0700

    update and delete category

--- Queries/CategoryQuery.cs
+++ Queries/CategoryQuery.cs
@@ -3,9 +3,39 @@ namespace TeguhJaya.Api.Queries
     public static class CategoryQuery
     {
         public const string qGetCategories = @"
-            SELECT id, name, image_base64, description
+            SELECT id, name, image_base64 AS ImageBase64, description
             FROM categories
             WHERE row_status = 0
+        ";
+
+        public const string qGetTotal = @"
+            SELECT COUNT(*)
+            FROM categories
+            WHERE row_status = 0
+        ";
+
+        public const string qCreateCategory = @"
+            INSERT INTO categories (name, image_base64, description)
+            VALUES (@name, @imageBase64, @description)
+            RETURNING id, name, image_base64, description
+        ";
+
+        public const string qDeleteCategory = @"
+            UPDATE categories
+            SET row_status = 1
+            WHERE id = @id
+        ";
+
+        public const string qGetCategory = @"
+            SELECT id, name, image_base64 AS ImageBase64, description
+            FROM categories
+            WHERE id = @id
+        ";
+
+        public const string qUpdateCategory = @"
+            UPDATE categories
+            SET name = @name, image_base64 = @imageBase64, description = @description
+            WHERE id = @id
         ";
     }
 }
--- Repositories/CategoryRepository.cs
+++ Repositories/CategoryRepository.cs
@@ -18,5 +18,45 @@ namespace TeguhJaya.Api.Repositories
                 return connection.Query<Category>(CategoryQuery.qGetCategories).ToList();
             }
         }
+
+        public int GetTotal()
+        {
+            using (var connection = CreateConnection())
+            {
+                return connection.ExecuteScalar<int>(CategoryQuery.qGetTotal);
+            }
+        }
+
+        public Category CreateCategory(Category category)
+        {
+            using (var connection = CreateConnection())
+            {
+                return connection.QuerySingleOrDefault<Category>(CategoryQuery.qCreateCategory, category);
+            }
+        }
+
+        public int DeleteCategory(int id)
+        {
+            using (var connection = CreateConnection())
+            {
+                return connection.ExecuteScalar<int>(CategoryQuery.qDeleteCategory, new {id});
+            }
+        }
+
+        public Category? GetCategory(int id)
+        {
+            using (var connection = CreateConnection())
+            {
+                return connection.QueryFirstOrDefault<Category>(CategoryQuery.qGetCategory, new {id});
+            }
+        }
+
+        public int UpdateCategory(Category category)
+        {
+            using (var connection = CreateConnection())
+            {
+                return connection.ExecuteScalar<int>(CategoryQuery.qUpdateCategory, category);
+            }
+        }
     }
 }
